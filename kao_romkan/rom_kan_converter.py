e61ea5a08de2a14b352c6938b385bfd80deb5376
Chris Loew <cloew123@gmail.com>
2016-07-18 18:49:34 -0600

    Added basic Double consonant stuff

--- kao_romkan/rom_kan_converter.py
+++ kao_romkan/rom_kan_converter.py
@@ -1,4 +1,4 @@
-from .symbols import HIRAGANA_MAP, COMPOUND_HIRAGANA_MAP
+from .symbols import HIRAGANA_MAP, COMPOUND_HIRAGANA_MAP, DOUBLE_CONS_HIRAGANA_MAP, DOUBLE_CONS_COMPOUND_HIRAGANA_MAP
 
 from cached_property import cached_property
 from kao_symbols import convert
@@ -12,14 +12,14 @@ class RomKanConverter:
         
     def convert(self, word):
         """ Convert the given Word """
-        return convert(word, self.symbolMap)
+        return convert(word, self.symbolMaps)
         
     @cached_property
-    def symbolMap(self):
-        """ Return the map of symbols that should be converted """
-        map = {}
-        if {'ゃ', 'ゅ', 'ょ'} & self.symbols:
-            map.update({key:value for key, value in COMPOUND_HIRAGANA_MAP.items() if key[0] not in self.symbols or key[1] not in self.symbols})
-        map.update({key:value for key, value in HIRAGANA_MAP.items() if key not in self.symbols})
-        return map
+    def symbolMaps(self):
+        """ Return the maps of symbols that should be converted """
+        maps = [{key:value for key, value in DOUBLE_CONS_COMPOUND_HIRAGANA_MAP.items() if not set(key).issubset(self.symbols)},
+                {key:value for key, value in DOUBLE_CONS_HIRAGANA_MAP.items() if not set(key).issubset(self.symbols)},
+                {key:value for key, value in COMPOUND_HIRAGANA_MAP.items() if not set(key).issubset(self.symbols)},
+                {key:value for key, value in HIRAGANA_MAP.items() if key not in self.symbols}]
+        return maps
         
--- kao_romkan/symbols.py
+++ kao_romkan/symbols.py
@@ -2,9 +2,16 @@
 HIRAGANA = "あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもやゆよらりるれろわをん"
 
 HIRAGANA_MAP = {'あ':'a', 'い':'i', 'う':'u', 'え':'e', 'お':'o', 'っ':"'", 'を':'wo', 'ん':'n'}
-                
+CONS_HIRAGANA_MAP = {
+    'き':'ki', 'ぎ':'gi',
+    'し':'shi', 'じ':'ji',
+    'ち':'chi'
+}
 COMPOUND_HIRAGANA_MAP = {
     'きゃ':'kya', 'ぎゃ':'gya',
     'しゅ':'shu', 'じゅ':'ju',
     'ちょ':'cho'
 }
+
+DOUBLE_CONS_HIRAGANA_MAP = {'っ'+key:value[0]+value for key, value in CONS_HIRAGANA_MAP.items()}
+DOUBLE_CONS_COMPOUND_HIRAGANA_MAP = {'っ'+key:value[0]+value for key, value in COMPOUND_HIRAGANA_MAP.items()}
