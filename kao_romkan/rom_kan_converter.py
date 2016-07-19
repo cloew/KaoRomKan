668446d01a08a252da15ace7269542325f72ae34
Chris Loew <cloew123@gmail.com>
2016-07-18 18:05:45 -0600

    Added basic compound hiragana functionality

--- kao_romkan/rom_kan_converter.py
+++ kao_romkan/rom_kan_converter.py
@@ -1,4 +1,4 @@
-from .symbols import HIRAGANA_MAP
+from .symbols import HIRAGANA_MAP, COMPOUND_HIRAGANA_MAP
 
 from cached_property import cached_property
 from kao_symbols import convert
@@ -17,5 +17,9 @@ class RomKanConverter:
     @cached_property
     def symbolMap(self):
         """ Return the map of symbols that should be converted """
-        return {key:value for key, value in HIRAGANA_MAP.items() if key not in self.symbols}
+        map = {}
+        if {'ゃ', 'ゅ', 'ょ'} & self.symbols:
+            map.update({key:value for key, value in COMPOUND_HIRAGANA_MAP.items() if key[0] not in self.symbols or key[1] not in self.symbols})
+        map.update({key:value for key, value in HIRAGANA_MAP.items() if key not in self.symbols})
+        return map
         
