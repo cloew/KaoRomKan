6f5c3ad8caef140d2ef99265fce1d5439e201d29
Chris Loew <cloew123@gmail.com>
2016-07-27 17:47:59 -0600

    Version Bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='kao_romkan',
-      version='0.1.2',
+      version='0.1.3',
       description="",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
